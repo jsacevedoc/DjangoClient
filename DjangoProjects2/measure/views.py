ab901c1b7ffdf9a49c04bfdcd3274816c9b9cd18
jsacevedoc <jsacevedoc@eafit.edu.co>
2020-05-19 18:06:27 -0500

    Taller 4 cliente

--- DjangoProjects2/measure/views.py
+++ DjangoProjects2/measure/views.py
@@ -25,7 +25,7 @@ def measure(request):
     return render(request, "measure/measure.html", {'measures': measures})
 
 def cultivo(request):
-    if 'codigo' in request.GET:
+    if 'area' in request.GET:
         codigo = request.GET['codigo']
         latitud = request.GET['latitud']
         longitud = request.GET['longitud']
@@ -33,7 +33,7 @@ def cultivo(request):
         area = request.GET['area']
 
         # Verifica si el value no esta vacio
-        if codigo:
+        if area:
             # Crea el json para realizar la petición POST al Web Service
             args = {'codigo': codigo, 'latitud': latitud, 'longitud': longitud, 'producto': producto, 'area': area}
             print(args)
